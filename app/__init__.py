1bcae5e75a47c4268dd395359ebd677326a32666
Stanislav161 <nfgor161@gmail.com>
2025-07-15 20:31:12 +0300

    add3

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-
+from flask_login import LoginManager, UserMixin  # Импортирован UserMixin
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'ваш_секретный_ключ'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clicker.db'
-
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login.view = 'login'
-
-from app import routes, models
+# Инициализация LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'  # Указываем имя функции представления для входа
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    clicks = db.Column(db.Integer, default=0)  # Поле для хранения кликов
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, flash
 from app import app, db
-from app.models import User
+from flask_login import login_user, logout_user, login_required, current_user
+
+# Добавляем маршруты
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Временная заглушка - замените реальной логикой
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/game')
+@login_required
+def game():
+    return "Страница игры - требуется авторизация"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
